b8c98c38cf23672033e9ff2a5f760fbddc9373eb
Maciej Rajewski <rajewski.ma@gmail.com>
2020-07-21 21:44:46 +0200

    Updates

---
+++ precache-manifest.a2b5cd2ce5b535484bcdf238d67f3a06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be7d3c50bb6b26e03a8a7ee774e76485",
+    "url": "/shop-task/index.html"
+  },
+  {
+    "revision": "d93f94855b5a4f1c1051",
+    "url": "/shop-task/static/css/main.04a7f981.chunk.css"
+  },
+  {
+    "revision": "dafa03e8dd52d25b1063",
+    "url": "/shop-task/static/js/2.261bc0d4.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/shop-task/static/js/2.261bc0d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d93f94855b5a4f1c1051",
+    "url": "/shop-task/static/js/main.6529cf6b.chunk.js"
+  },
+  {
+    "revision": "ea27e40b43a92d2a6ba1",
+    "url": "/shop-task/static/js/runtime-main.e92f4f60.js"
+  }
+]);
